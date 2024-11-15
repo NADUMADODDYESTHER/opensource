e2f5cd2894d21295cd9dc14963363aa8e1166cc1
NADUMADODDYESTHER <163634099+NADUMADODDYESTHER@users.noreply.github.com>
2024-11-15 14:26:05 +0530

    Create R_P_S.py

---
+++ visaa_prep/R_P_S.py
@@ -0,0 +1,11 @@
+Vignesh, Charan = input().split()
+if Vignesh == Charan:
+    print("NULL")
+elif Vignesh == 'R' and Charan == 'S':
+    print("Vignesh")
+elif Vignesh == 'S' and Charan == 'P':
+    print("Vignesh")
+elif Vignesh == 'P' and Charan == 'R':
+    print("Vignesh")
+else:
+    print("Charan")
